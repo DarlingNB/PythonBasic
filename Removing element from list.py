7bdb4d1335489ac62e9fe1502e380eed272fa286
DarlingNB <darlingnibo@gmail.com>
2023-02-20 16:01:34 +0200

    Changed code, because misunderstood the problem

--- Removing element from list.py
+++ Removing element from list.py
@@ -1,11 +1,22 @@
 new_list_element = []
 list = []
+
 while (new_list_element := int(input("Вводите число (0 - конец):"))) != 0:
     list.append(new_list_element)
-list.pop()
 k = int(input("Введите индекс необходимого числа \n"))
+
+# Второй вариант
+list.insert(len(list),list[k])
 list.remove(list[k])
+list.pop()
 print(list)
+
+# Первый вариант
+#k_item = (list[k])
+#list.append(k_item)
+#list.remove(k_item)
+#list.pop()
+#print(list)
 
 
 

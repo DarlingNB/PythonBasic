4a4c15917f2ee8bfc3d51031ab5e074032552a77
DarlingNB <darlingnibo@gmail.com>
2023-03-02 12:49:54 +0200

    Fix task

--- expandet form.py
+++ expandet form.py
@@ -1,9 +1,11 @@
 # number = input("Input number\n")
-number = 70304
+number = 0
 
 def expanded_form(number):
     num_str = str(number)
     result = []
+    if int(number) <= 0:
+        raise Exception("You must input number bigger them 0")
     for i in range(len(num_str)):
         if num_str[i] != '0':
             result.append(num_str[i] + '0' * (len(num_str) - i - 1))
